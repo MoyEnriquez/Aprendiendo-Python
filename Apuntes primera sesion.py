b231b0f58319ace2c7d379fa6571b9cd5f6645a8
Moises Alejandro Enriquez Vargas <moises.enriquez@ciencias.unam.mx>
2021-11-21 16:37:26 -0600

    Ejercicio y nuevos ocmandos 1

--- Apuntes primera sesion.py
+++ Apuntes primera sesion.py
@@ -1,2 +1,23 @@
 Lista = [321,143,3425,345,456,657]
+
+print(Lista)
+
+
+#%% <- Crea y delimita celdas para trabajar con bloques en Spyder
+#print <- Sirve para que ciertas cosas aparezcan en la consola
+
+Lista2 = ["Juan", "Paco", "Pedro", "de la mar"]
+print (Lista2)
+
+#Para listas numericas se pueden utilizar estas funciones
+len (Lista)
+sum (Lista)
+max (Lista)
+min (Lista)
+
+#Ahora, para poder verlas en la consola podemos hacer una lista grandota con esos valores o poner 5 prints
+print([len (Lista),sum (Lista),max (Lista),min (Lista)])
+
+#Algunos metodos comunes en las listas
+Lista.append(125)
 print(Lista)
